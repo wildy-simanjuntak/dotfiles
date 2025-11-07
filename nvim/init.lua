73e8cb7f5d1a1f1c1c0448ac34a1985f20de8cf1
wildy-simanjuntak <simanjuntakwildy@gmail.com>
2025-11-07 11:06:41 +0700

    updated nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,167 @@
--- Tambah lazy.nvim ke runtime path
-vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Options
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.completeopt = "menuone,noselect"
 
--- Inisialisasi Lazy dan daftar plugin
+-- Plugins
 require("lazy").setup({
-  -- contoh plugin
+
+  -- Theme
   {
-    "nvim-tree/nvim-tree.lua",                -- file explorer kayak VSCode
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("nvim-tree").setup()
+      vim.cmd("colorscheme tokyonight-night")
     end,
   },
 
+  -- File Explorer
   {
-    "akinsho/bufferline.nvim",                -- tab file kayak VSCode
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+
+  -- Buffer Tabs
+  {
+    "akinsho/bufferline.nvim",
     version = "*",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("bufferline").setup({
+        options = { separator_style = "slant" },
+      })
+    end,
+  },
+
+  -- Finder
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup()
+      vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>")
+      vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>")
+    end,
+  },
+
+  -- Syntax Highlight
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "javascript", "typescript", "html", "css", "python", "c", "cpp" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+
+  -- Language Server
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local servers = { "ts_ls", "pyright", "lua_ls", "clangd", "html", "cssls" }
+
+      for _, server in ipairs(servers) do
+        vim.lsp.config[server] = { capabilities = capabilities }
+        vim.lsp.enable(server)
+      end
+    end,
+  },
+
+  -- Status Line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
         options = {
-          separator_style = "slant",
+          theme = "tokyonight",
+          section_separators = { left = "", right = "" },
+          component_separators = "|",
         },
       })
     end,
   },
+
+  -- Auto Pairs
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  },
+
 })
+
+-- Keymaps
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>q", ":bd<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>x", ":x<CR>")
 
